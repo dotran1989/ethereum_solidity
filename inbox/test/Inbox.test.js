bfd2d174c9f498d98d674eb93731fcb4559c7edc
cuongquoc2710 <cuongdnq@proteusion.com>
2018-03-07 22:19:56 +0700

    get list accounts

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,7 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor
 const web3 = new Web3(ganache.provider());
 
-class Car {
+/* class Car {
     park() {
         return 'stopped';
     }
@@ -27,4 +27,15 @@ describe('Car', () => {
     it('can drive', () => {
         assert.equal(car.drive(), "vroom");
     })
+}); */
+
+beforeEach(() => {
+    // get a list of all accounts
+    web3.eth.getAccounts().then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    })
 });
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {})
+})
